127586e8a1986c7c0fe3c4ee74d40bb3e79f68a7
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-03-31 11:15:37 -0700

    delete comments

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -5,13 +5,9 @@ class Hamming
     raise ArgumentError if first_strand.length != second_strand.length
 
     hamming_distance = 0
-    first_strand.size.times {|i| hamming_distance += 1 if first_strand[i] != second_strand[i]}
+    first_strand.length.times {|i| hamming_distance += 1 if first_strand[i] != second_strand[i]}
 
     return hamming_distance
   end
 
 end
-
-# maybe I should split the characters?
-# first = first_strand.split("")
-# second = second_strand.split("")
