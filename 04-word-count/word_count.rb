70a01eefdb9215a07d4bfd252419baf4f1002084
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-08 20:29:03 -0700

    word count test works

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,13 @@
-
+  def words(phrase)
+    return "0" if phrase == nil
+    words = phrase.split()
+    counts = Hash.new(0)
+    words.each do |word|
+      if counts.has_key?(word)
+        counts[word] +=1
+      else
+        counts.store(word, 1)
+      end
+    end
+    counts
+  end
